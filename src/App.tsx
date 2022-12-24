cd4e73e8c98486e5647c6abc23ef3284909f3cbc
Anna Metsäpelto <anna.metsapelto@gmail.com>
2022-12-24 18:30:27 +0200

    Fetch all products and display in console

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import { useAppSelector } from './hooks/reduxHook'
+import { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from './hooks/reduxHook'
+import { fetchAllProducts } from './redux/reducers/productReducer';
 
 const App = () => {
   const products = useAppSelector(state => state.productReducer);
+  const dispatch = useAppDispatch();
   console.log(products);
+
+  useEffect(() => {
+    dispatch(fetchAllProducts());
+  }, [])
   return (
     <div>App</div>
   )
--- src/redux/reducers/productReducer.ts
+++ src/redux/reducers/productReducer.ts
@@ -1,13 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { ProductType } from '../../types/ProductType'; 
 
 const initialState: ProductType[]  = []
+
+export const fetchAllProducts = createAsyncThunk(
+    "fetchAllProducts",
+    async() => {
+        try {
+            const dataAsJson = await fetch("https://api.escuelajs.co/api/v1/products");
+            const data = await dataAsJson.json();
+            return data;
+        } catch (e: any) {
+            throw new Error(e.message);
+        }
+    }
+)
 
 const productSlice = createSlice({ 
     name: "productSlice",
     initialState: initialState,
     reducers: {
-
+        addAll: (state, action) => {}
+        },
+    extraReducers: (build) => {
+        build.addCase(fetchAllProducts.fulfilled, (state, action) => {
+            return action.payload;
+        } )
     }
 })
 
