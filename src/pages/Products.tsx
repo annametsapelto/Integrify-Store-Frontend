b397abed00d60b81a70f202b14b4c1a60ea02ba1
Anna Metsäpelto <anna.metsapelto@gmail.com>
2023-01-02 18:39:52 +0200

    Style products page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../hooks/reduxHook'
 import { fetchAllProducts, sortByNameAsc, sortByNameDesc, sortByPriceAsc, sortByPriceDesc, createProduct } from '../redux/reducers/productReducer';
 import { CreatedProductType, ProductType } from '../types/ProductType';
+import SearchIcon from '@mui/icons-material/Search';
+import { TextField, InputLabel } from '@mui/material';
 
 const Products = () => {
     const [toBeSearched, setToBeSearched] = useState("");
@@ -50,24 +52,27 @@ const Products = () => {
     }
   
     return(
+      <div className='products'>
+          <h1>Products</h1>
+          <div>
+            <InputLabel htmlFor='search'>Search for products</InputLabel>
+            <TextField  type="text" name="search" id='search' value={toBeSearched} onChange={(event) => setToBeSearched(event.target.value)} variant="outlined"></TextField>
+            <SearchIcon fontSize='large'></SearchIcon>
+          </div>
         <div>
-            <h1>Products</h1>
-            <div>
-        <label htmlFor='search'>Search for products</label>
-        <input type="text" name="search" id='search' value={toBeSearched} onChange={(event) => setToBeSearched(event.target.value)}></input>
+          <div className='products_filterButtons'>
+            <button onClick={sortHandlerAsc}>Sort A to Z</button>
+            <button onClick={sortHandlerDesc}>Sort Z to A</button>
+            <button onClick={sortHandlerPriceAsc}>Sort by Cheapest</button>
+            <button onClick={sortHandlerPriceDesc}>Sort by Most Expensive</button>
+          </div>
+          <ul>
+            {products.map(product => (<li key={product.id} ><Link to={String(product.id)}>{product.title}  {product.price} € <button onClick={(event) => seeDetailsHandler(product)}>See details</button>
+            </Link></li>))}
+          </ul>
+          <button onClick={createHandler}>Create product</button>
+        </div>
       </div>
-      <div>
-        <button onClick={sortHandlerAsc}>Sort A to Z</button>
-        <button onClick={sortHandlerDesc}>Sort Z to A</button>
-        <button onClick={sortHandlerPriceAsc}>Sort by Cheapest</button>
-        <button onClick={sortHandlerPriceDesc}>Sort by Most Expensive</button>
-        <ul>
-          {products.map(product => (<li key={product.id} ><Link to={String(product.id)}>{product.title}  {product.price} € <button onClick={(event) => seeDetailsHandler(product)}>See details</button>
-          </Link></li>))}
-        </ul>
-        <button onClick={createHandler}>Create product</button>
-      </div>
-    </div>
     )
 }
 
