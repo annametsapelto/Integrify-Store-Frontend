a991b4f6c100cab058843cff706eea9044179e7c
Anna Metsäpelto <anna.metsapelto@gmail.com>
2023-01-03 11:41:28 +0200

    Work on registering and profile

--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const MyProfile = () => {
     return(
-        <div>
-            <h1>My profile</h1>
+        <div className="profile">
+            <div className="profile_logged">
+                <h1>My profile</h1>
+                <p>Here is your profile information:</p>
+            </div>
+            <div>
+                <h1>You are not logged in</h1>
+                <p>Please log in to see your information.</p>
+                <p>If you have not yet registered an account, you can do it <Link to="../register">here</Link>.</p>
+            </div>
+            
         </div>
+
     )
 }
 
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,29 @@
+import { useState } from 'react';
+
+import { Input, InputLabel } from '@mui/material';
+import { RegisterType } from '../types/RegisterType';
+
 const Register = () => {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event: React.FormEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    }
+
     return(
         <div>
             <h1>Register here</h1>
+            <form>
+                <InputLabel htmlFor='username'>Your username</InputLabel>
+                <Input id="username" name="username" type="string" required value={username} onChange={(event) => setUsername(event.target.value)}></Input>
+                <InputLabel htmlFor='userEmail'>Your email</InputLabel>
+                <Input type="email" id="userEmail" name="userEmail" required value={email} onChange={(event) => setEmail(event.target.value)}></Input>
+                <InputLabel htmlFor='userPassword'>Your password</InputLabel>
+                <Input type='string' id='userPassword' name='userPassword' required value={password} onChange={(event) => setPassword(event.target.value)}></Input>
+                <button onSubmit={(event) => handleSubmit(event)}>Register</button>
+            </form>
         </div>
     )
 }
